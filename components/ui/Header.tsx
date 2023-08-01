960d4f00d4a94b0ca4834147e5791856b375088c
guitavano <tavano62@gmail.com>
2023-07-31 22:13:56 -0300

    header

---
+++ components/ui/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "deco-sites/std/components/Image.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+
+export interface Props {
+  logo: LiveImage;
+  alt: string;
+}
+
+export default function Header({ logo, alt }: Props) {
+  return (
+    <div class="container px-12 py-7 flex justify-center">
+      <Image
+        src={logo}
+        width={200}
+        alt={alt}
+      />
+    </div>
+  );
+}
--- live.gen.ts
+++ live.gen.ts
@@ -11,10 +11,11 @@ import * as $$$$$0 from "./islands/Form.tsx";
 import * as $$$$$1 from "./islands/WhatsAppButton.tsx";
 import * as $$$$$$$$0 from "./sections/DealerHome.tsx";
 import * as $$$$$$$$1 from "./sections/DealerList.tsx";
-import * as $$$$$$$$2 from "./sections/LeadfyLogo.tsx";
-import * as $$$$$$$$3 from "./sections/SectionList.tsx";
-import * as $$$$$$$$4 from "./sections/StoresPdp.tsx";
-import * as $$$$$$$$5 from "./sections/Theme.tsx";
+import * as $$$$$$$$2 from "./sections/Header.tsx";
+import * as $$$$$$$$3 from "./sections/LeadfyLogo.tsx";
+import * as $$$$$$$$4 from "./sections/SectionList.tsx";
+import * as $$$$$$$$5 from "./sections/StoresPdp.tsx";
+import * as $$$$$$$$6 from "./sections/Theme.tsx";
 import * as $live_workflows from "$live/routes/live/workflows/run.ts";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
@@ -196,10 +197,11 @@ const manifest = {
     "$live/sections/UseSlot.tsx": i2$$$$$4,
     "deco-sites/leadfy-dealers/sections/DealerHome.tsx": $$$$$$$$0,
     "deco-sites/leadfy-dealers/sections/DealerList.tsx": $$$$$$$$1,
-    "deco-sites/leadfy-dealers/sections/LeadfyLogo.tsx": $$$$$$$$2,
-    "deco-sites/leadfy-dealers/sections/SectionList.tsx": $$$$$$$$3,
-    "deco-sites/leadfy-dealers/sections/StoresPdp.tsx": $$$$$$$$4,
-    "deco-sites/leadfy-dealers/sections/Theme.tsx": $$$$$$$$5,
+    "deco-sites/leadfy-dealers/sections/Header.tsx": $$$$$$$$2,
+    "deco-sites/leadfy-dealers/sections/LeadfyLogo.tsx": $$$$$$$$3,
+    "deco-sites/leadfy-dealers/sections/SectionList.tsx": $$$$$$$$4,
+    "deco-sites/leadfy-dealers/sections/StoresPdp.tsx": $$$$$$$$5,
+    "deco-sites/leadfy-dealers/sections/Theme.tsx": $$$$$$$$6,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$$5,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$$6,
     "deco-sites/std/sections/configNuvemShop.tsx": i2$$$$$7,
