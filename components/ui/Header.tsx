a57411f98b7005a685b6743f11d976cf72e1e049
guitavano <tavano62@gmail.com>
2023-08-08 22:54:48 -0300

    add track gtm at header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,21 +1,27 @@
 import Image from "deco-sites/std/components/Image.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
+import GoogleTagManager from "partytown/integrations/GTM.tsx";
+
 export interface Props {
   logo: LiveImage;
   alt: string;
+  trackingId?: string;
 }
 
-export default function Header({ logo, alt }: Props) {
+export default function Header({ logo, alt, trackingId }: Props) {
   return (
-    <div class="container px-12 py-7 flex justify-center">
-      <a href="/">
-        <Image
-          src={logo}
-          width={200}
-          alt={alt}
-        />
-      </a>
-    </div>
+    <>
+      <div class="container px-12 py-7 flex justify-center">
+        <a href="/">
+          <Image
+            src={logo}
+            width={200}
+            alt={alt}
+          />
+        </a>
+      </div>
+      {trackingId && <GoogleTagManager trackingId={trackingId} />}
+    </>
   );
 }
