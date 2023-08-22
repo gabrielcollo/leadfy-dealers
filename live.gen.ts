75c8d5758877c2cf03210ec01038b00b7a8ac341
guitavano <tavano62@gmail.com>
2023-08-21 21:33:16 -0300

    force deploy

--- live.gen.ts
+++ live.gen.ts
@@ -23,18 +23,6 @@ import * as $$$$$$$$7 from "./sections/RichText.tsx";
 import * as $$$$$$$$8 from "./sections/SectionList.tsx";
 import * as $$$$$$$$9 from "./sections/StoresPdp.tsx";
 import * as $$$$$$$$10 from "./sections/Theme.tsx";
-import * as $live_workflows from "$live/routes/live/workflows/run.ts";
-import * as $live_middleware from "$live/routes/_middleware.ts";
-import * as $live_workbench from "$live/routes/live/workbench.ts";
-import * as $live_invoke from "$live/routes/live/invoke/index.ts";
-import * as $live_invoke_key from "$live/routes/live/invoke/[...key].ts";
-import * as $live_editorData from "$live/routes/live/editorData.ts";
-import * as $live_inspect from "$live/routes/live/inspect/[...block].ts";
-import * as $live_release from "$live/routes/live/release.ts";
-import * as $live_meta from "$live/routes/live/_meta.ts";
-import * as $live_previews_block from "$live/routes/live/previews/[...block].tsx";
-import * as $live_previews_index from "$live/routes/live/previews/index.tsx";
-import * as $live_catchall from "$live/routes/[...catchall].tsx";
 import { DecoManifest } from "$live/types.ts";
 import * as i2$$$0 from "$live/loaders/secret.ts";
 import * as i2$$$1 from "$live/loaders/state.ts";
@@ -214,20 +202,7 @@ const manifest = {
   },
   "routes": {
     "./routes/_app.tsx": $$$$2,
-    "./routes/_middleware.ts": $live_middleware,
-    "./routes/[...catchall].tsx": $live_catchall,
     "./routes/api/airtable.ts": $$$$0,
-    "./routes/index.tsx": $live_catchall,
-    "./routes/live/_meta.ts": $live_meta,
-    "./routes/live/editorData.ts": $live_editorData,
-    "./routes/live/inspect/[...block].ts": $live_inspect,
-    "./routes/live/invoke/[...key].ts": $live_invoke_key,
-    "./routes/live/invoke/index.ts": $live_invoke,
-    "./routes/live/previews/[...block].tsx": $live_previews_block,
-    "./routes/live/previews/index.tsx": $live_previews_index,
-    "./routes/live/release.ts": $live_release,
-    "./routes/live/workbench.ts": $live_workbench,
-    "./routes/live/workflows/run.ts": $live_workflows,
     "./routes/styles.css.ts": $$$$1,
   },
   "islands": {
--- main.ts
+++ main.ts
@@ -3,14 +3,16 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="esnext" />
 
-import { $live } from "$live/mod.ts";
-import manifest from "./live.gen.ts";
 import { start } from "$fresh/server.ts";
+import { $live } from "$live/mod.ts";
+import plugins from "deco-sites/std/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
+import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
 
 await start($live(manifest, site), {
   plugins: [
+    ...plugins(),
     partytownPlugin(),
   ],
 });
