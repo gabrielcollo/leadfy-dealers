fa986dc4dec19db941234865d471be44a1b28d27
guitavano <tavano62@gmail.com>
2023-09-04 18:11:30 -0300

    add loading at whatsapp handle

--- components/ui/WhatsAppModal.tsx
+++ components/ui/WhatsAppModal.tsx
@@ -24,6 +24,8 @@ export default function WhatsAppModal(
   const nome = useSignal("");
   const telefone = useSignal("");
 
+  const loading = useSignal(false);
+
   const isMouseDown = useSignal(false);
   const initX = useSignal(0);
   const initY = useSignal(0);
@@ -158,6 +160,7 @@ export default function WhatsAppModal(
               id="wpp-form"
               class="flex flex-col gap-2 items-end"
               onSubmit={(e) => {
+                loading.value = true;
                 saveLead();
                 e.preventDefault();
               }}
@@ -200,24 +203,31 @@ export default function WhatsAppModal(
               <div class="">
                 <button
                   type="submit"
-                  class="transition-all bg-[#0c6156] border border-[#0c6156] text-white font-semibold rounded px-6 py-3 flex items-center gap-2"
+                  disabled={loading.value}
+                  class="transition-all bg-[#0c6156] border border-[#0c6156] text-white font-semibold rounded px-6 py-3"
                 >
-                  Iniciar conversa
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="white"
-                    height="16px"
-                    width="16px"
-                    version="1.1"
-                    id="Layer_1"
-                    viewBox="0 0 512.001 512.001"
-                  >
-                    <g>
-                      <g>
-                        <path d="M483.927,212.664L66.967,25.834C30.95,9.695-7.905,42.023,1.398,80.368l21.593,89.001    c3.063,12.622,11.283,23.562,22.554,30.014l83.685,47.915c6.723,3.85,6.738,13.546,0,17.405l-83.684,47.915    c-11.271,6.452-19.491,17.393-22.554,30.015l-21.594,89c-9.283,38.257,29.506,70.691,65.569,54.534l416.961-186.83    C521.383,282.554,521.333,229.424,483.927,212.664z M359.268,273.093l-147.519,66.1c-9.44,4.228-20.521,0.009-24.752-9.435    c-4.231-9.44-0.006-20.523,9.434-24.752l109.37-49.006l-109.37-49.006c-9.44-4.231-13.665-15.313-9.434-24.752    c4.229-9.44,15.309-13.666,24.752-9.435l147.519,66.101C373.996,245.505,374.007,266.49,359.268,273.093z" />
-                      </g>
-                    </g>
-                  </svg>
+                  {loading.value
+                    ? <div>Aguarde...</div>
+                    : (
+                      <div class="flex items-center gap-2">
+                        Iniciar conversa
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="white"
+                          height="16px"
+                          width="16px"
+                          version="1.1"
+                          id="Layer_1"
+                          viewBox="0 0 512.001 512.001"
+                        >
+                          <g>
+                            <g>
+                              <path d="M483.927,212.664L66.967,25.834C30.95,9.695-7.905,42.023,1.398,80.368l21.593,89.001    c3.063,12.622,11.283,23.562,22.554,30.014l83.685,47.915c6.723,3.85,6.738,13.546,0,17.405l-83.684,47.915    c-11.271,6.452-19.491,17.393-22.554,30.015l-21.594,89c-9.283,38.257,29.506,70.691,65.569,54.534l416.961-186.83    C521.383,282.554,521.333,229.424,483.927,212.664z M359.268,273.093l-147.519,66.1c-9.44,4.228-20.521,0.009-24.752-9.435    c-4.231-9.44-0.006-20.523,9.434-24.752l109.37-49.006l-109.37-49.006c-9.44-4.231-13.665-15.313-9.434-24.752    c4.229-9.44,15.309-13.666,24.752-9.435l147.519,66.101C373.996,245.505,374.007,266.49,359.268,273.093z" />
+                            </g>
+                          </g>
+                        </svg>
+                      </div>
+                    )}
                 </button>
               </div>
             </form>
