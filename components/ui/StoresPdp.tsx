fb5ccba0b9a950aa7f42439e545643786ec5c61c
Gabriel Collo <67707416+gabrielcollo@users.noreply.github.com>
2025-03-13 10:02:57 -0300

    Update StoresPdp.tsx

--- components/ui/StoresPdp.tsx
+++ components/ui/StoresPdp.tsx
@@ -25,7 +25,7 @@ export interface WhatsNormalButton {
 
 export interface Props {
   whatsNormalButton: WhatsNormalButton;
-  showPrice: boolean;
+  showPrice?: boolean;
   showPriceText?: boolean;
   priceText?: string;
   vehicleDescription?: HTML;
